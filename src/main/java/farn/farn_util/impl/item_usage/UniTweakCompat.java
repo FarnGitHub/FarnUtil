2c8054cfbd8fe6c556bd0fc7c9aaf506bb72b716
Farn <93525542+FarnGitHub@users.noreply.github.com>
2026-02-13 17:26:37 +0700

    3.2-babric2

--- src/main/java/farn/farn_util/impl/item_usage/UniTweakCompat.java
+++ src/main/java/farn/farn_util/impl/item_usage/UniTweakCompat.java
@@ -1,7 +1,7 @@
 package farn.farn_util.impl.item_usage;
 
 import farn.farn_util.api.item_usage.FovHandler;
-//import net.danygames2014.unitweaks.event.RegisterUniTweaksCompatEvent;
+import net.danygames2014.unitweaks.event.RegisterUniTweaksCompatEvent;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.mine_diver.unsafeevents.listener.EventListener;
@@ -10,11 +10,11 @@ import net.minecraft.client.Minecraft;
 @SuppressWarnings("unused")
 public class UniTweakCompat {
 
-    /*@Environment(EnvType.CLIENT)
+    @Environment(EnvType.CLIENT)
     @EventListener
     public void registerFov(RegisterUniTweaksCompatEvent event) {
         event.registerFovCompat((fov, tick) -> fov * getFovMulti());
-    }*/
+    }
 
     @Environment(EnvType.CLIENT)
     private float getFovMulti() {
