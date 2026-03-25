686c6a787a0ff8995a3d35b934b39cb121c17b14
Farn <93525542+FarnGitHub@users.noreply.github.com>
2026-03-25 15:38:36 +0700

    3.3.1

--- src/main/java/farn/farn_util/api/item_usage/ActionHandler.java
+++ src/main/java/farn/farn_util/api/item_usage/ActionHandler.java
@@ -2,6 +2,7 @@ package farn.farn_util.api.item_usage;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
 import net.modificationstation.stationapi.api.util.Identifier;
 
 public abstract class ActionHandler {
@@ -9,11 +10,12 @@ public abstract class ActionHandler {
     public final Identifier id;
 
     @Environment(EnvType.CLIENT)
-    public final ActionAnimator animation;
+    public ActionAnimator animation;
 
     public ActionHandler(Identifier id) {
         this.id = id;
-        animation = createAnimation();
+        if(FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT)
+            animation = createAnimation();
     }
 
     @Environment(EnvType.CLIENT)
