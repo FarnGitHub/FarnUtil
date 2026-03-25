fb0ca0842c549e89d16f6e4b3df9e25eee327b35
Farn <93525542+FarnGitHub@users.noreply.github.com>
2026-03-25 16:27:26 +0700

    3.3.2

--- src/main/java/farn/farn_util/mixin/item_usage/client/MixinFovMulti.java
+++ src/main/java/farn/farn_util/mixin/item_usage/client/MixinFovMulti.java
@@ -1,15 +1,12 @@
 package farn.farn_util.mixin.item_usage.client;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import farn.farn_util.api.item_usage.FovHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GameRenderer.class)
 public class MixinFovMulti {
@@ -17,8 +14,8 @@ public class MixinFovMulti {
     @Shadow
     private Minecraft client;
 
-    @Inject(method="getFov", at = @At("RETURN"))
-    public void overrideFov(float tick, CallbackInfoReturnable<Float> cir, @Local(index = 3) LocalFloatRef fov) {
-        fov.set(fov.get() * FovHandler.getFovWithCheck(client.player));
+    @ModifyReturnValue(method="getFov", at = @At("RETURN"))
+    public float test(float original) {
+        return original *  FovHandler.getFovWithCheck(client.player);
     }
 }
